ff266ed0b2a4488c6f7245e65d83b47a25751f87
LazySoftware1 <alexvalpro@gmail.com>
2023-07-17 15:47:33 +0600

    homework4

---
+++ app_lesson_4/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import lesson_4
+
+urlpatterns = [
+    path('', lesson_4)
+]
---
+++ app_lesson_4/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def lesson_4(request):
+    return HttpResponse('Домашка по 4 занятию')
+
+# Create your views here.
