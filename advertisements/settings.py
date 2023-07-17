63c98d066da7adb12be9751c677b3c8e0d362fa2
LazySoftware1 <alexvalpro@gmail.com>
2023-07-17 15:53:37 +0600

    homework4

--- advertisements/settings.py
+++ advertisements/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_advertisements',
     'app_lesson_4'
 ]
 
--- advertisements/urls.py
+++ advertisements/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app_advertisements.urls')),
     path('lesson_4', include('app_lesson_4.urls'))
 ]
