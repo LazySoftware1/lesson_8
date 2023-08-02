154e0f92a8678b540b6d9e8e31468acacce76a77
LazySoftware1 <alexvalpro@gmail.com>
2023-08-03 01:10:21 +0600

    homework7

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Advertisements
-
-class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'price', 'created_at', 'auction']
-
-admin.site.register(Advertisements, AdvertisementAdmin)
 
 # Register your models here.
