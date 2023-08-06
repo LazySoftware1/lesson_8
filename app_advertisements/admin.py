43976fffd416fd29aaf35dbbf584e92f4bf58beb
LazySoftware1 <alexvalpro@gmail.com>
2023-08-06 11:01:51 +0600

    homework_8

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Advertisements
 
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'price', 'created_date', 'update_date', 'auction']
+    list_filter = ['auction', 'created_at']
 
+admin.site.register(Advertisements, AdvertisementAdmin)
 # Register your models here.
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+
 
 class Advertisements(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -10,6 +12,30 @@ class Advertisements(models.Model):
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold">'
+                'Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d:%m:%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def update_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold">'
+                'Сегодня в {}</span>', update_time
+            )
+        return self.update_at.strftime('%d:%m:%Y в %H:%M:%S')
 
     class Meta:
         db_table = "advertisements"
